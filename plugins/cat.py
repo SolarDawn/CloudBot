af0302ceb4fe9468438e430f5a41b4aef7c8727e
Fox <billiamfo@gmail.com>
2017-12-18 13:22:12 -0500

    organize config, add affection

--- plugins/cat.py
+++ plugins/cat.py
@@ -2,23 +2,31 @@ import os
 import codecs
 import random
 import re
+import json
 
 from cloudbot import hook
 
 @hook.on_start()
 def load_actions(bot):
-    path = os.path.join(bot.data_dir, "cat_actions.txt")
+    path = os.path.join(bot.data_dir, "cat.json")
     global cat_actions
     with codecs.open(path, encoding="utf-8") as f:
-        cat_actions = [line.strip() for line in f.readlines() if not line.startswith("//")]
+        cat_actions = json.load(f)
 
-greeting_re = re.compile(r'(hello|hi) (catbot|kitty)(!)?', re.I)
-@hook.regex(greeting_re)
+@hook.regex(r':3')
+def catface(match, nick, action):
+    action("boops " + nick + " :3")
+
+@hook.regex(r'(?i)(hello|hi) (catbot|kitty)(!)?')
 def greeting(match):
     return ":3 meow!"
 
-beckon_re = re.compile(r'(?:(come( here)|here|hey) (catbot|kitty))|(?:(hey )?(catbot|kitty),? (come( here)|here))', re.I)
-@hook.regex(beckon_re)
+#beckon_re = re.compile(r'(?:(come( here)|here|hey) (catbot|kitty))|(?:(hey )?(catbot|kitty),? (come( here)|here))', re.I)
+@hook.regex(r'(?i)(?:(come( here)|here|hey) (catbot|kitty))|(?:(hey )?(catbot|kitty),? (come( here)|here))')
 def beckon(match, nick, action):
-    response = random.choice(cat_actions).replace("<nick>", nick)
+    response = random.choice(cat_actions['greetings']).replace("<nick>", nick)
     action(response)
+
+@hook.regex(r'(?i)(?:pets|rubs|scratches) (?:catbot|kitty)')
+def affection(match, action):
+    action(random.choice(cat_actions['happy_actions']) + " happily")
