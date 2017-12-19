db5c400808b49a9afe656b3cdb50a37db1d4b31f
Fox <billiamfo@gmail.com>
2017-12-19 15:58:37 -0500

    added feeding, sleepiness

--- plugins/pet.py
+++ plugins/pet.py
@@ -9,6 +9,10 @@ from sqlalchemy import Table, Column, PrimaryKeyConstraint, String
 from cloudbot import hook
 from cloudbot.util import colors, timeformat, database
 
+#MAX_HUNGER = 360
+MAX_HUNGER = 2
+MAX_TIREDNESS = 2
+
 pet_table = Table(
     "pets",
     database.metadata,
@@ -21,16 +25,28 @@ pet_table = Table(
 pets = {}
 
 class Pet:
-    def __init__(self, name, owner, pet_type):
+    def __init__(self, name, owner, species):
         self.name = name
         self.owner = owner
-        self.pet_type = pet_type
+        self.species = species
+        
+        self.hunger = 0
+        self.begging = False
+        self.beg_delay = 0
+        
+        self.play_counter = 0
+        
+        self.tiredness = 0
+        self.sleeping = False
+        self.sleep_delay = 0
+        
 
     def get_action(self, action_type):
-        if self.pet_type in pet_actions and action_type in pet_actions[self.pet_type]:
-            return random.choice(pet_actions[self.pet_type][action_type])
+        if self.species in pet_actions and action_type in pet_actions[self.species]:
+            return random.choice(pet_actions[self.species][action_type])
         else:
             return random.choice(pet_actions['pet'][action_type])
+
 
 @hook.on_start()
 def load_pets(bot, db):
@@ -61,12 +77,12 @@ def addpet(event, db, nick, text):
         # add
         newpet = Pet(args[0], nick, args[1])
         pets[newpet.name] = newpet
-        db.execute(pet_table.insert().values(pet_name = newpet.name, owner_name = newpet.owner, pet_type = newpet.pet_type))
+        db.execute(pet_table.insert().values(pet_name = newpet.name, owner_name = newpet.owner, pet_type = newpet.species))
         db.commit()
-        return "Added new " + newpet.pet_type + " " + newpet.name
+        return "Added new " + newpet.species + " " + newpet.name
 
 
-@hook.command("removepet", "rpet")
+@hook.command("removepet", "rempet", "rpet")
 def removepet(event, db, nick, text):
     """[pet name] - removes a pet that belongs to you"""
     args = text.split(" ")
@@ -92,8 +108,8 @@ def removepet(event, db, nick, text):
 def listpets():
     #"""lists all the pets"""
     outstr = "Pets: "
-    for i in pets:
-        outstr += pets[i].name + "(" + pets[i].pet_type + "), "
+    for name, pet in pets.items():
+        outstr += name + "(" + pet.species + "), "
     
     if outstr.endswith(", "):
         return outstr[:-2]
@@ -118,6 +134,27 @@ def affection(match, nick, message):
         message("\x1D*" + cur_pet.name + " " + response + "*\x0F")
 
 
+feed_re = re.compile(r'feeds (\w+)|fills (\w+)(?:\'s)? (?:food|(?:bowl|dish))|gives (\w+) (?:some )?food', re.I)
+@hook.regex(feed_re)
+def feed(match, nick, message):
+    pet_name = ""
+    for group in match.groups():
+        if group != None:
+            pet_name = group
+    
+    if pet_name in pets:
+        cur_pet = pets[pet_name]
+        if cur_pet.hunger >= MAX_HUNGER * 3/4:
+            # hungry enough to eat
+            cur_pet.hunger = 0
+            response = cur_pet.get_action('eat_actions').replace("<nick>", nick)
+            message("\x1D*" + cur_pet.name + " " + response + "*\x1D")
+            return
+        else:
+            # not hungry
+            message("\x1D*" + cur_pet.name + " sniffs at their food and walks away*\x1D")
+
+
 @hook.irc_raw("PRIVMSG")
 def parse_actions(irc_raw, message):
     i = irc_raw.find(":\x01ACTION")
@@ -137,3 +174,43 @@ def parse_actions(irc_raw, message):
         if (match):
             affection(match, nick, message)
             return
+        
+        match = feed_re.match(text)
+        if (match):
+            feed(match, nick, message)
+            return
+
+
+@hook.periodic(60)
+def update_pet_states():
+    for name, pet in pets.items():
+        if pet.hunger < MAX_HUNGER:
+            pet.hunger += 1
+        else:
+            if not pet.begging:
+                pet.begging = True
+                pet.beg_delay = random.randint(0, 9)
+            elif pet.beg_delay > 0:
+                # countdown to begging
+                pet.beg_delay -= 1
+            else:
+                # time to beg
+                pet.begging = False
+                response = pet.get_action("beg_actions").replace("<nick>", pet.owner)
+                message("\x1D*" + name + " " + response + "*\x1D")
+        return
+        
+        if pet.sleeping:
+            # sleeping
+            if pet.tiredness > 0:
+                pet.tiredness -= 1
+            else:
+                pet.sleeping = False
+                pet.tiredness = 0
+        
+        else:
+            # awake
+            pet.tiredness += 1
+            
+    
+    return
